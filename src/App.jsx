30f801254b944ecf86af3d2a8b299881ac10459e
itachisky <akashsky2107@gmail.com>
2024-06-17 22:15:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        Hello world
+      </div>
+    </>
+  )
+}
+
+export default App
